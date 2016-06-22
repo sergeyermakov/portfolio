81d5c1602b2696c7d5c6315646f1ba9c2d8c9020
Sergey Ermakov <xom94ok@gmail.com>
2016-06-22 14:55:08 +0300

    added main menu

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,11 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+	$(document).ready(function() {
+		$('.nav-icon').click(function() {
+			$(this).toggleClass('open');
+			$('.main-menu').toggleClass('open');
+		});
+	});
+
 })();
